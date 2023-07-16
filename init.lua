c22632324a648f1a9815a0117c4c4f063538f85f
bohi <boydor808@gmail.com>
2023-07-16 13:19:17 +0800

    Update option configuration

--- init.lua
+++ init.lua
@@ -1,5 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.cmd("set tabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set expandtab")
+-- vim.cmd("set tabstop=8")
+-- vim.cmd("set shiftwidth=8")
+-- vim.cmd("set expandtab")
